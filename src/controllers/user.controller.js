463a75a9d8a73c6ed2d915f8c1411b96bc5a3cbb
DBMhatre <deva.mhatre@buzzybrains.com>
2023-12-20 13:58:29 +0530

    add register controller & bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,76 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiErrors.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
+  // 1. Get User Details from Front End
+  // 2. Validation of the User Details… (Not empty , Check email format)
+  // 3. Check if user already exists : (username, email)
+  // 4. Check for images (Avatar)
+  // 5. Upload them to Cloudinary (Get the URL)
+  // 6. Create user Obj : (Create entry in db)
+  // 7. Remove the suspecious fields from the Response to user (Password , Refresh Token)
+  // 8. Check if user is created
+  // 9. return response!
+
+  const { fullName, email, username, password } = req.body;
+
+  // 1) 2)
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required!");
+  }
+
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
+
+  // 3)
+  if (existedUser) {
+    throw new ApiError(409, "User with email or password already exists!");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  // 4)
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  // 5)
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required!");
+  }
+
+  // 6)
+  const user = await User.create({
+    fullName,
+    email,
+    password,
+    username: username.toLowerCase(),
+    avatar: avatar?.url,
+    coverImage: coverImage?.url || "",
   });
+
+  // 7) 8)
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering the user!");
+  }
+
+  // 9)
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered successfully!"));
 });
 
 export { registerUser };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,30 +1,28 @@
-import {v2 as cloudinary} from cloudinary
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
 
 cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
 
-const uploadOnCloudinary = async (localFilePath)=>{
-    try {
-        if(!localFilePath) return null;
+    //Upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
 
-        //Upload file on cloudinary
-        const response = await cloudinary.uploader.upload(
-            localFilePath, {resource_type : "auto"}
-        )
+    //file uploaded successfully
+    console.log("File uploaded using Cloudinary...!", response.url);
 
-        //file uploaded successfully
-        console.log("File uploaded using Cloudinary...!",response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //as opration failed we remove the File on our server
+  }
+};
 
-        return response;
-        
-    } catch (error) {
-        fs.unlinkSync(localFilePath)//as opration failed we remove the File on our server
-    }
-}
-
-export {uploadOnCloudinary}
+export { uploadOnCloudinary };
